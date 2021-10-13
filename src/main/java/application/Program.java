9d431d7a369840b953a9bcec4079c21e91847625
unknown <marcosshuichi@gmail.com>
2021-10-13 17:08:21 -0300

    Class created

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,10 +1,13 @@
 package application;
 
+import boardgame.Board;
+import boardgame.Position;
+
 public class Program {
 
     public static void main(String[] args) {
 
-        System.out.println("hello");
+        Board board = new Board(8, 8);
     }
 
 }
